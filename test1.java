9a496b7c3d2022d0437e14e62a6623318d23931a
WasayefAnanzeh <61361902+WasayefAnanzeh@users.noreply.github.com>
2020-03-11 09:34:26 +0200

    testing Ex

---
+++ test1.java
@@ -0,0 +1,17 @@
+package lecture1_Prog2;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Programming 2");
+		System.out.println("Done");
+		int x = 4;
+		int y = 5;
+		int sum = x + y;
+		
+		System.out.println( "the"+sum+"additon              " +    sum);
+
+	}
+
+}
